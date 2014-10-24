8c04a2c91581b0027ddb6d98c11a7bc881d2c5b5
jeroen188 <jeroen188@gmail.com>
2014-10-23 23:59:30 -0300

    Initial commit

---
+++ app/src/main/java/com/primo5/habits/DailyTasks.java
@@ -0,0 +1,124 @@
+package com.primo5.habits;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+
+public class DailyTasks extends Activity {
+
+    DBAdapter myDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_tasks);
+
+        openDB();
+        populateListViewFromDB();
+        registerListClickCallback();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        closeDB();
+    }
+
+    private void openDB() {
+        myDb = new DBAdapter(this);
+        myDb.open();
+    }
+
+    private void closeDB() {
+        myDb.close();
+    }
+
+    /*
+     * UI Button Callbacks
+     */
+
+    public void onClick_ClearAll(View v) {
+        myDb.deleteAll();
+        populateListViewFromDB();
+    }
+
+
+    private void populateListViewFromDB() {
+        Cursor cursor = myDb.getAllRows();
+
+        // Allow activity to manage lifetime of the cursor.
+        // DEPRECATED! Runs on the UI thread, OK for small/short queries.
+        startManagingCursor(cursor);
+
+        // Setup mapping from cursor to view fields:
+        String[] fromFieldNames = new String[]
+                {DBAdapter.KEY_TASK, DBAdapter.KEY_DIMENSION, DBAdapter.KEY_MONTH};
+        int[] toViewIDs = new int[]
+                {R.id.textTask,     R.id.textDimension,           R.id.textMonth};
+
+        // Create adapter to may columns of the DB onto elemesnt in the UI.
+        SimpleCursorAdapter myCursorAdapter =
+                new SimpleCursorAdapter(
+                        this,		// Context
+                        R.layout.activity_daily_detail,	// Row layout template
+                        cursor,					// cursor (set of DB records to map)
+                        fromFieldNames,			// DB Column names
+                        toViewIDs				// View IDs to put information in
+                );
+
+        // Set the adapter for the list view
+        ListView myList = (ListView) findViewById(R.id.taskView);
+        myList.setAdapter(myCursorAdapter);
+    }
+
+    private void registerListClickCallback() {
+        ListView myList = (ListView) findViewById(R.id.taskView);
+        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked,
+                                    int position, long idInDB) {
+
+                updateItemForId(idInDB);
+                displayToastForId(idInDB);
+            }
+        });
+    }
+
+    private void updateItemForId(long idInDB) {
+        Cursor cursor = myDb.getRow(idInDB);
+        if (cursor.moveToFirst()) {
+            long idDB = cursor.getLong(DBAdapter.COL_ROWID);
+            String task = cursor.getString(DBAdapter.COL_TASK);
+            String dimension = cursor.getString(DBAdapter.COL_DIMENSION);
+            String month = cursor.getString(DBAdapter.COL_MONTH);
+
+            month += "!";
+            myDb.updateRow(idInDB, task, dimension, month);
+        }
+        cursor.close();
+        populateListViewFromDB();
+    }
+
+    private void displayToastForId(long idInDB) {
+        Cursor cursor = myDb.getRow(idInDB);
+        if (cursor.moveToFirst()) {
+            long idDB = cursor.getLong(DBAdapter.COL_ROWID);
+            String task = cursor.getString(DBAdapter.COL_TASK);
+            String dimension = cursor.getString(DBAdapter.COL_DIMENSION);
+            String month = cursor.getString(DBAdapter.COL_MONTH);
+
+            String message = "ID: " + idDB + "\n"
+                    + "Task: " + task + "\n"
+                    + "Dimension: " + dimension + "\n"
+                    + "Month: " + month;
+            Toast.makeText(DailyTasks.this, message, Toast.LENGTH_LONG).show();
+        }
+        cursor.close();
+    }
+}
+
---
+++ app/src/main/java/com/primo5/habits/NewTask.java
@@ -0,0 +1,54 @@
+package com.primo5.habits;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.database.Cursor;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class NewTask extends Activity {
+    DBAdapter myDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_task);
+        openDB();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        closeDB();
+    }
+
+
+    private void openDB() {
+        myDb = new DBAdapter(this);
+        myDb.open();
+    }
+    private void closeDB() {
+        myDb.close();
+    }
+
+    public void Onclick_task(View v) {
+        EditText task = (EditText)findViewById(R.id.editTask);
+        EditText dimension = (EditText)findViewById(R.id.editDimension);
+        EditText month = (EditText)findViewById(R.id.editMonth);
+
+        String getTask = task.getText().toString();
+        String getDimension= dimension.getText().toString();
+        String getMonth = month.getText().toString();
+
+        long newId = myDb.insertRow(getTask, getDimension, getMonth);
+        Toast.makeText(getApplicationContext(), "Task added!!! =)",
+                Toast.LENGTH_LONG).show();
+
+    }
+
+    }
+
